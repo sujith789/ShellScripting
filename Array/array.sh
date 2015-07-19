ff3edd67a4a4a096abe29406415bcab5c1e58b7a
sujithmo <sujithperuva@yahoo.com>
2015-07-19 12:57:10 -0700

    First commit

---
+++ Array/array.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+NAME[0]="Sachin"
+NAME[1]="Ganguly"
+NAME[2]="Dravid"
+NAME[3]="Kohli"
+
+echo "First Name = ${NAME[0]}"
+echo "Second Name = ${NAME[1]}"
---
+++ Basic/arithmetic_operators.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+a=25
+b=10
+
+sum=`expr $a + $b`
+echo  " Sum = ${sum}"
+
+div=`expr $a / $b`
+echo " Div = $div"
